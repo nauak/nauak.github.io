a281eae1ed9fd52b715cf5bc2697cdbd1d77d1db
marcexpositg <marcexgoy@gmail.com>
2018-11-23 00:37:05 -0500

    23th remove sw

--- js/activateSW.js
+++ js/activateSW.js
@@ -1,11 +1,11 @@
 //This file should be mentioned in all .html pages of our site.
 //Alternatively its code can be inserted inline with <script> tags in the .htmls
 
-if ('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
-        .then(function(){
-            console.log('SW registered');
-        });
-};
+//if ('serviceWorker' in navigator){
+//    navigator.serviceWorker.register('/sw.js')
+//        .then(function(){
+//            console.log('SW registered');
+//        });
+//};
 //This way the serviceworker is only used if the navigator is compatible, if not, it will not throw an error
 //because use executes serviceworker if the object serviceworker exists
